2c377202953239f3a7f5b497115f389a47d1f641
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-01-09 15:54:40 -0600

    adding todo tasks to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
+const toDoData = [
+  { thingName: 'Wake up' },
+  { thingName: 'Morning Routine' },
+  { thingName: 'School' },
+  { thingName: 'Lunch' },
+  { thingName: 'Project' },
+  { thingName: 'Stand Up' },
+  { thingName: 'Night Routine' },
+  { thingName: 'Sleep' }
+];
+
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!!!</h2>
+        <h2>Welcome to your Todo App!!!!</h2>
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+// you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
